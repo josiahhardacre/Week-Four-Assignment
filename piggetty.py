904f106d19112e309828f80a7d6ca9de2b89ebd9
josiahhardacre <josiah.hardacre@gmail.com>
2015-10-07 16:16:03 +0000

    WeekFor

--- piggetty.py
+++ piggetty.py
@@ -1,7 +1,7 @@
-#
-# File Header
-#
-# Add your name to this
+# Josiah Hardacre Collaborated with Nicole Ignasiak, Nathan Pugh, and Trevor Waite
+# CIS 125 82A
+# 9/30/15
+# Week 4 Lab
 
 # Define a function called piggy(string) that returns a string
 
@@ -14,21 +14,41 @@ def piggy(word):
 	return pig
 
 # Open the file *getty.txt* for reading.  
-
+gettyfile = open("getty.txt", "r")
 # Open a new file *piggy.txt* for writing.  
-
+piggyfile = open("piggy.txt", "w")
 # Read the getty.txt file into a string.  
-
+address = gettyfile.read()
 # Strip out bad characters (, - .).  
+address = address.replace(".","")
+address = address.replace(",","")
+address = address.replace("-","")
 
+print (address)
 # Split the string into a list of words.  
-
+listaddress = address.split()
 # Create a new empty string.  
-
+piggystring = ""
 # Loop through the list of words, pigifying each one.  
+vowels = "aeiouAEIOU"     
+piggynewword = ""
 
-# Add the pigified word (and a space) to the new string.  
-
+#Loops through every word in the Gettysburg Address
+for word in listaddress:
+	if word[0] in vowels:
+		piggynewword = word + "yay"
+		piggystring = piggystring + " " + piggynewword
+	else:
+		n=0
+		for letter in word:
+			if letter in vowels:
+# Add the pigified word (and a space) to the new string.
+				piggynewword = (word[n:] + word[0:n] + "ay")
+				piggystring = piggystring + " " + piggynewword
+				break
+			n=1
 # Write the new string to piggy.txt.  
-
+piggyfile.write(piggystring)
 # close the files.
+piggyfile.close()
+gettyfile.close()
--- pigify.py
+++ pigify.py
@@ -3,21 +3,26 @@
 #
 # Define vowels
 
-vowels = "aeiouAEIOU"
+vowels = "aeiouAEIOU"         #I solved the case sensitivity by adding the vowels in both lowercase and uppercase.
 
 # Ask for word
 
 word = input("Please enter a word: ")
 
+if (originalword[0] in vowels):
+    print((originalword + endofvowel).capitalize())
+    
+else:
+    print((originalword[1:] + originalword[0] + endofconsonant).capitalize())
 # Loop through word, one letter at a time
 
 for letter in word:
-	# Check if letter is a vowel
 	if letter in vowels:
-		# True?  We are done
 		pig = word + "yay"
 	else:
 		# False? Consonant
 		pig = word[1:] + word[0] + "ay"
-		
+
+
 print(pig)
+
